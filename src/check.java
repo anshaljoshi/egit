561af88e08f056fd4748f71c3abd4837c2ec54d1
anshaljoshi <anshal.joshi@upr.edu>
2016-05-26 10:37:06 -0400

    mid way

---
+++ src/check.java
@@ -0,0 +1,26 @@
+
+public class check {
+	public static void main(String[] args)
+	{
+		int c=1;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<5;j++)
+            {
+            System.out.print(" ");
+            }
+             
+            for(int k=1;k<=c;k++)
+            {
+                if(k%2==0)
+                	System.out.print(" ");
+                else
+                	System.out.print("*");
+            }
+            System.out.println("\n");
+        c+=2;
+        }
+	}
+
+
+}
